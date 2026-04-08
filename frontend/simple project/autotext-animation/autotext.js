b918a01e67680c02230008b2e26645bf930a2aeb
guruvardhan-tech-village <guruvardhaniniot@gmail.com>
2026-04-08 12:43:07 +0530

    update

--- frontend/simple project/autotext-animation/autotext.js
+++ frontend/simple project/autotext-animation/autotext.js
@@ -1,11 +1,13 @@
 const containerEl = document.querySelector(".container");
-const carrers = ["Web Developer", "IT Designer", "Freelancer", "Content Creator","Architecture Designer", "UI/UX Designer", "Graphic Designer", "Game Developer", "Mobile App Developer", "Data Scientist"];
+const carrers = ["Web Developer", "IT Designer", "Freelancer", "Content Creator","Architecture Designer", "UI/UX Designer", "Graphic Designer", "Game Developer", "Mobile App Developer", "Data Scientist",
+    "Electrical Engineer", "Mechanical Engineer", "Civil Engineer", "Chemical Engineer", "Aerospace Engineer", "Biomedical Engineer", "Environmental Engineer", "Software Engineer", "Network Engineer", 
+    "Database Administrator","Digital Marketer", "Social Media Manager", "SEO Specialist", "Content Strategist", "Copywriter", "Brand Manager", "Public Relations Specialist","Financial Analyst", "Investment Banker", "Accountant", "Auditor", "Financial Planner", "Risk Manager","Teacher", "Professor", "Researcher", "Librarian","Doctor", "Nurse", "Pharmacist","Lawyer", "Judge","Chef","Artist","Musician","Writer","Actor"];
 
 let carrersIndex = 0;
 let charIndex = 0;
 function updateText() {
     
-    containerEl.innerHTML = `<h1> I am  ${carrers[carrersIndex].slice(0,1)=== "I" || carrers[carrersIndex].slice(0,1)=== "U" || carrers[carrersIndex].slice(0,1)=== "A" ? "an" : "a"} ${carrers[carrersIndex].slice(0, charIndex)} </h1>`;
+    containerEl.innerHTML = `<h1> I am  ${carrers[carrersIndex].slice(0,1)=== "I" || carrers[carrersIndex].slice(0,1)=== "U" || carrers[carrersIndex].slice(0,1)=== "A"|| carrers[carrersIndex].slice(0,1)=== "O" || carrers[carrersIndex].slice(0,1)=== "E" ? "an" : "a"} ${carrers[carrersIndex].slice(0, charIndex)} </h1>`;
     charIndex++;
     setTimeout(updateText, 150);
     if(charIndex === carrers[carrersIndex].length + 1) {
